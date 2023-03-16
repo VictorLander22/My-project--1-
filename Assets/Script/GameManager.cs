8350aaa796eafac53ef09c6b974c9674ecb41cd4
VictorLander22 <victordlander@hotmail.com>
2023-03-16 17:40:42 -0300

    Teste Unity

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject hazardPrefab;
+
+    void Start()
+    {
+        StartCoroutine(SpawnHazards());
+    }
+
+    private IEnumerator SpawnHazards()
+    {
+        
+        var x =Random.Range(-4,9);
+        Instantiate(hazardPrefab, new Vector3(x, 23, 0), Quaternion.identity);
+        yield return null;
+    }
+}
